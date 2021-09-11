24508a7cae5e646186ce74376e603536bbe1d9a2
Kalkidan Demes <kalkidandemes99@gmail.com>
2021-09-11 08:45:53 -0700

    fix code

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,5 +7,8 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_world():
-  """ hello hbnb """
-    return 'Hello HBNB!'
+    """ hello hbnb """
+        return 'Hello HBNB!'
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
