526fdb38e57b4d8fb9d06f308f802179c43c96a8
Kalkidan Demes <kalkidandemes99@gmail.com>
2021-09-12 00:57:45 -0700

    Update state.py

--- models/state.py
+++ models/state.py
@@ -12,3 +12,16 @@ class State(BaseModel, Base):
 
     name = Column(String(128), nullable=False)
     cities = relationship('City', back_populates='state', cascade='delete')
+
+    if models.storage_type != "db":
+        @property
+        def cities(self):
+            """
+            getter for cities
+            """
+        cities = []
+        insta = models.storage.all(City)
+        for value in insta.values():
+            if value.state_id == self.id:
+                cities.append(value)
+        return cities
